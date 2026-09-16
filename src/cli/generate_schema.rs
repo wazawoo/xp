842388b5114cd33340f2cff20088d36dc7ff6060
wazawoo <reed.moseng@gmail.com>
2026-09-16 14:49:38 -0700

    Fix todo, clean up

--- src/cli/generate_schema.rs
+++ src/cli/generate_schema.rs
@@ -7,18 +7,16 @@ use crate::xp::Xp;
 
 #[derive(Args, Debug)]
 pub struct GenerateSchemaArgs {
-    /// path to generate schema at
+    /// path for generated json schema
     #[arg(short, long, default_value = "xp.schema.json")]
     schema_path: PathBuf,
 }
 
 pub fn run(args: &GenerateSchemaArgs) -> Result<(), Box<dyn Error>> {
     let schema = schema_for!(Xp);
-    // todo: handle to_str error
-    let schema_path = args.schema_path.to_str().unwrap();
     let schema_json = serde_json::to_string_pretty(&schema)?;
-    info!("Xp schema: {}", schema_json);
-    std::fs::write(schema_path, schema_json)?;
-    info!("wrote schema file to {}", schema_path);
+    info!("xp schema: {}", schema_json);
+    std::fs::write(args.schema_path.clone(), schema_json)?;
+    info!("wrote schema file to {}", args.schema_path.display());
     Ok(())
 }
